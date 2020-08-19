2b6ce718655f20aecd3096463c58669276b284f0
Włodzimierz Kowjako <kowyako@yandex.ru>
2020-08-19 21:50:56 +0200

    Add new control
    SendFile control to show that you have received or sended a file

---
+++ ChatBubble/SendFile.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ChatBubble
+{
+    partial class SendFile
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SendFile));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(242, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(104, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "256 KB";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(61, 19);
+            this.label2.MaximumSize = new System.Drawing.Size(178, 15);
+            this.label2.MinimumSize = new System.Drawing.Size(178, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ssdsds";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(104, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Attachment";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // SendFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "SendFile";
+            this.Size = new System.Drawing.Size(242, 80);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ChatBubble/SendFile.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace ChatBubble
+{
+    public partial class SendFile : UserControl
+    {
+        public SendFile()
+        {
+            InitializeComponent();
+        }
+        public string FileName
+        {
+            get
+            {
+                return label2.Text;
+            }
+            set
+            {
+                label2.Text = value;
+            }
+        }
+        public string FileSize
+        {
+            get
+            {
+                return label3.Text;
+            }
+            set
+            {
+                label3.Text = Ext.ToPrettySize(Convert.ToInt64(value));
+            }
+        }
+        Label time = new Label();
+        public void AddTimeLabelSender()
+        {
+            panel1.BackColor = Color.White;
+            time.Left = panel1.Width - 35;
+            time.Top = panel1.Height + 5;
+            time.Text = DateTime.Now.ToShortTimeString();
+            time.Font = new Font("Segoe UI", 9);
+            time.ForeColor = Color.DarkRed;
+            this.Controls.Add(time);
+        }
+        private void PictureBox1_get(object sender, EventArgs e)
+        {
+            Process.Start(filename);
+        }
+        public static FileStream fs { private get; set; }
+        public string OpenFileDialog()
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Title = "Choose a file";
+            fd.ShowDialog();
+            fs = (FileStream)fd.OpenFile();          
+            return fs.Name;
+        }
+        public string SetFile()
+        {
+            string path = OpenFileDialog();
+            return path;
+        }
+        public void ResetStream()
+        {
+            fs.Close();
+            fs = null;
+        }
+        string filename = "";
+        public void SetFile(string path)
+        {
+            filename = path;
+        }
+        public void AddTimeLabelGetter()
+        {
+            panel1.BackColor = Color.SkyBlue;
+            time.Left = 5;
+            time.Top = panel1.Height + 5;
+            time.Text = DateTime.Now.ToShortTimeString();
+            time.Font = new Font("Segoe UI", 9);
+            time.ForeColor = Color.DarkRed;
+            this.Controls.Add(time);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start(filename);
+        }
+    }
+}
