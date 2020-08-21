26660a33f1f5977b3b725f654f48843879383951
Włodzimierz Kowjako <kowyako@yandex.ru>
2020-08-21 18:14:49 +0200

    Add new control SendAudio
    Now you can send AudioFiles

---
+++ ChatBubble/SendAudio.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ChatBubble
+{
+    partial class SendAudio
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SendAudio));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(44, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Voice Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(44, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "00:15";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(145, 37);
+            this.panel1.TabIndex = 3;
+            // 
+            // SendAudio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "SendAudio";
+            this.Size = new System.Drawing.Size(145, 61);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ ChatBubble/SendAudio.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ChatBubble
+{
+    public partial class SendAudio : UserControl
+    {
+        public SendAudio()
+        {
+            InitializeComponent();
+        }
+        Label time = new Label();
+        public void AddTimeLabelSender()
+        {
+            panel1.BackColor = Color.White;
+            label1.BackColor = Color.White;
+            label2.BackColor = Color.White;
+            pictureBox1.BackColor = Color.White;
+            time.Left = panel1.Width - 35;
+            time.Top = panel1.Height + 5;
+            time.Text = DateTime.Now.ToShortTimeString();
+            time.Font = new Font("Segoe UI", 9);
+            time.ForeColor = Color.DarkRed;
+            this.Controls.Add(time);
+        }
+        public void AddTimeLabelGetter()
+        {
+            panel1.BackColor = Color.SkyBlue;
+            label1.BackColor = Color.SkyBlue;
+            label2.BackColor = Color.SkyBlue;
+            pictureBox1.BackColor = Color.SkyBlue;
+            time.Left = 5;
+            time.Top = panel1.Height + 5;
+            time.Text = DateTime.Now.ToShortTimeString();
+            time.Font = new Font("Segoe UI", 9);
+            time.ForeColor = Color.DarkRed;
+            this.Controls.Add(time);
+        }
+        public string AudioTime
+        {
+            get
+            {
+                return label2.Text;
+            }
+            set
+            {
+                label2.Text = value;
+            }
+        }
+        public void SetFile(string path)
+        {
+            filename = path;
+        }
+        private string filename;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start(filename);
+        }
+    }
+}
