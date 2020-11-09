000fa17f0577684bc722f237584233e61311aeec
Vladimir <kowyako@yandex.ru>
2020-11-09 17:20:53 +0200

    Add new component
    SendVideo component

---
+++ ChatBubble/SendVideo.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ChatBubble
+{
+    partial class SendVideo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SendVideo));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(138, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(43, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Video Message";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SendVideo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "SendVideo";
+            this.Size = new System.Drawing.Size(138, 61);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ChatBubble/SendVideo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ChatBubble
+{
+    public partial class SendVideo : UserControl
+    {
+        public SendVideo()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.Opaque, true);
+        }
+        TransparentLabel time = new TransparentLabel();
+        public void AddTimeLabelSender()
+        {
+            time.BackColor = System.Drawing.Color.Transparent;
+            panel1.BackColor = Color.White;
+            label1.BackColor = Color.White;
+            pictureBox1.BackColor = Color.White;
+            time.Left = panel1.Width - 35;
+            time.Top = panel1.Height + 5;
+            time.Text = DateTime.Now.ToShortTimeString();
+            time.Font = new Font("Segoe UI", 9);
+            time.ForeColor = Color.DarkRed;
+            this.Controls.Add(time);
+        }
+        public void AddTimeLabelGetter()
+        {
+            time.BackColor = System.Drawing.Color.Transparent;
+            panel1.BackColor = Color.SkyBlue;
+            label1.BackColor = Color.SkyBlue;
+            pictureBox1.BackColor = Color.SkyBlue;
+            time.Left = 5;
+            time.Top = panel1.Height + 5;
+            time.Text = DateTime.Now.ToShortTimeString();
+            time.Font = new Font("Segoe UI", 9);
+            time.ForeColor = Color.DarkRed;
+            this.Controls.Add(time);
+        }
+        public void SetFile(string path)
+        {
+            filename = path;
+        }
+        private string filename;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start(filename);
+        }
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        private int opacity = 0;
+        [DefaultValue(50)]
+        public int Opacity
+        {
+            get
+            {
+                return this.opacity;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentException("value must be between 0 and 100");
+                this.opacity = value;
+            }
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle = cp.ExStyle | WS_EX_TRANSPARENT;
+                return cp;
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            using (var brush = new SolidBrush(Color.FromArgb(this.opacity * 255 / 100, this.BackColor)))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+            base.OnPaint(e);
+        }
+    }
+}
